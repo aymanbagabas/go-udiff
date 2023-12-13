5e08ebf8e180601f5dd8120867756faa15a58ba2
Ayman Bagabas <ayman.bagabas@gmail.com>
2023-12-13 12:48:09 -0500

    feat: support number of context lines
    The default number of context lines is 3 and defined in
    `udiff.DefaultContextLines`
    
    Fixes: https://github.com/aymanbagabas/go-udiff/issues/5

--- export.go
+++ export.go
@@ -5,6 +5,6 @@ type UnifiedDiff = unified
 
 // ToUnifiedDiff takes a file contents and a sequence of edits, and calculates
 // a unified diff that represents those edits.
-func ToUnifiedDiff(fromName, toName string, content string, edits []Edit) (UnifiedDiff, error) {
-	return toUnified(fromName, toName, content, edits)
+func ToUnifiedDiff(fromName, toName string, content string, edits []Edit, contextLines int) (UnifiedDiff, error) {
+	return toUnified(fromName, toName, content, edits, contextLines)
 }
